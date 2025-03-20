18801aca7c09a0101d121b8864fad5f182d83ebf
makarm001477 <makarm.mek1000@gmail.com>
2025-03-20 23:11:11 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

---
+++ Assets/script/MoveTMPLeftRight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoveTMPLeftRight : MonoBehaviour
+{
+    public TextMeshProUGUI tmpText; // ลาก TMP มาใส่ใน Inspector
+    public float speed = 2f; // ความเร็วในการขยับ
+    public float distance = 100f; // ระยะทางที่ขยับไปมา
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = tmpText.rectTransform.anchoredPosition;
+    }
+
+    void Update()
+    {
+        float movement = Mathf.PingPong(Time.time * speed, distance * 2) - distance;
+        tmpText.rectTransform.anchoredPosition = new Vector3(startPosition.x + movement, startPosition.y, startPosition.z);
+    }
+}
---
+++ Assets/script/RandomTextColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RandomTextColor : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro; // ลาก TextMeshPro UI มาใส่ใน Inspector
+    private float changeInterval = 2f;
+
+    void Start()
+    {
+        InvokeRepeating("ChangeColor", 0f, changeInterval);
+    }
+
+    void ChangeColor()
+    {
+        textMeshPro.color = new Color(Random.value, Random.value, Random.value);
+    }
+}
+
